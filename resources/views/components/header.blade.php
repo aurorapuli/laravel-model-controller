f4cb07a34318b9df0a2ba6cc2279602da8516783
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-02-12 16:28:07 +0100

    add: movies

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,3 @@
 <header>
-    <h1>Hello from Header</h1>
+    <h1>Moovies</h1>
 </header>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,15 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <ul class="container d-flex row-cols-3 flex-wrap justify-content-between mt-5">
+        @foreach ($movies as $movie)
+            <li class="mb-4 ">
+                <div>Titolo: {{ $movie->title }}</div>
+                <div>Titolo Originale: {{ $movie->original_title }}</div>
+                <div>Nazionalita: {{ $movie->nationality }}</div>
+                <div>Data di uscita: {{ $movie->date }}</div>
+                <div>Voto: {{ $movie->vote }}</div>
+            </li>
+        @endforeach
+    </ul>
 @endsection
